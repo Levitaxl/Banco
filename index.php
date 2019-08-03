b2ec3601a8a821028c96cbb7647b5dde918d366d
unknown <hssanchez.17@est.ucab.edu.ve>
2019-08-03 11:48:35 +0200

    Termino la prueba

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
                         <li><a href="https://banco-program-2.herokuapp.com/Vista/InicioSesionVista.php">Iniciar Sesion</a></li>-->
                         <li><a href="http://localhost/PhpProject1/Vista/CreacionUsuarioVista.php">Crear cuenta</a></li>
                         <li><a href="http://localhost/PhpProject1/Vista/InicioSesionVista.php">Iniciar Sesion</a></li>
-                        <li><a href="http://localhost/PhpProject1/Vista/InicioSesionVista.php">Iniciar Sesion2</a></li>
+      
 			
 		</ul>
 	</div>
